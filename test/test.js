e4d3e60d6335b3d1be5d1719cc6deb345c3da9fc
syzer3@gmail.com <syzer3@gmail.com>
2015-11-11 20:42:54 +0100

    inlined functions in tests

--- test/test.js
+++ test/test.js
@@ -6,13 +6,13 @@ var q = require('bluebird')
 tape('postive / negative', function (t) {
   var nb = bayes(memdb())
 
-  nb.train('positive', 'amazing, awesome movie!! Yeah!! Oh boy.', function (err) {
+  nb.train('positive', 'amazing, awesome movie!! Yeah!! Oh boy.', err => {
     t.notOk(err, 'no err')
-    nb.train('positive', 'Sweet, this is incredibly, amazing, perfect, great!!', function (err) {
+    nb.train('positive', 'Sweet, this is incredibly, amazing, perfect, great!!', err => {
       t.notOk(err, 'no err')
-      nb.train('negative', 'terrible, shitty thing. Damn. Sucks!!', function (err) {
+      nb.train('negative', 'terrible, shitty thing. Damn. Sucks!!', err => {
         t.notOk(err, 'no err')
-        nb.classify('awesome, cool, amazing!! Yay.', function (err, category) {
+        nb.classify('awesome, cool, amazing!! Yay.', (err, category) => {
           t.same(category, 'positive', 'should be positive')
           t.end()
         })
@@ -24,13 +24,13 @@ tape('postive / negative', function (t) {
 tape('spam / not spam', function (t) {
   var nb = bayes(memdb())
 
-  nb.train('spam', 'Great viagra for you!', function (err) {
+  nb.train('spam', 'Great viagra for you!', err => {
     t.notOk(err, 'no err')
-    nb.train('not spam', 'UTOSC is the best conference ever!', function (err) {
+    nb.train('not spam', 'UTOSC is the best conference ever!', err => {
       t.notOk(err, 'no err')
-      nb.train('not spam', 'Some more strings that are not spam!', function (err) {
+      nb.train('not spam', 'Some more strings that are not spam!', err => {
         t.notOk(err, 'no err')
-        nb.classify('Great opportunity in Nigeria!', function (err, category) {
+        nb.classify('Great opportunity in Nigeria!', (err, category) => {
           t.same(category, 'spam', 'should be spam')
           t.end()
         })
@@ -39,7 +39,7 @@ tape('spam / not spam', function (t) {
   })
 })
 
-tape('train with promise', function (t) {
+tape('train with promise', (t) => {
   var nb = bayes(memdb())
 
   nb.trainAsync('positive', 'amazing, awesome movie!! Yeah!! Oh boy.')
@@ -50,14 +50,14 @@ tape('train with promise', function (t) {
       return nb.trainAsync('negative', 'terrible, shitty thing. Damn. Sucks!!');
     })
     .then(function () {
-      return nb.classify('awesome, cool, amazing!! Yay.', function (err, category) {
+      return nb.classify('awesome, cool, amazing!! Yay.', (err, category) => {
         t.same('positive', category, 'should be positive')
         t.end()
       })
     })
 })
 
-tape('que all train promises', function (t) {
+tape('que all train promises', (t) => {
   var nb = bayes(memdb())
 
   var thingsToDo = [
@@ -66,15 +66,15 @@ tape('que all train promises', function (t) {
     nb.trainAsync('negative', 'terrible, shitty thing. Damn. Sucks!!')
   ];
 
-  q.all(thingsToDo).then(function () {
-    return nb.classify('awesome, cool, amazing!! Yay.', function (err, category) {
+  q.all(thingsToDo).then(() => {
+    return nb.classify('awesome, cool, amazing!! Yay.', (err, category) => {
       t.same('positive', category, 'should be positive')
       t.end()
     })
   })
 })
 
-tape('classify can be used as promise', function (t) {
+tape('classify can be used as promise', (t) => {
   var nb = bayes(memdb())
 
   var thingsToDo = [
@@ -84,27 +84,25 @@ tape('classify can be used as promise', function (t) {
   ];
 
   q.all(thingsToDo)
-    .then(function () {
-      return nb.classifyAsync('awesome, cool, amazing!! Yay.')
-    })
-    .then(function (category) {
+    .then(() => nb.classifyAsync('awesome, cool, amazing!! Yay.'))
+    .then((category) => {
       t.same('positive', category, 'should be positive')
       t.end()
     })
 })
 
-tape('classifyLabels with three labels', function (t) {
+tape('classifyLabels with three labels', (t) => {
   var nb = bayes(memdb())
 
-  nb.train('spam', 'Great viagra for you!', function (err) {
+  nb.train('spam', 'Great viagra for you!', (err) => {
     t.notOk(err, 'no err')
-    nb.train('not spam', 'UTOSC is the best conference ever!', function (err) {
+    nb.train('not spam', 'UTOSC is the best conference ever!', (err) => {
       t.notOk(err, 'no err')
-      nb.train('dont know', 'It might be good opportunity.', function (err) {
+      nb.train('dont know', 'It might be good opportunity.', (err) => {
         t.notOk(err, 'no err')
-        nb.train('not spam', 'Some more strings that are not spam!', function (err) {
+        nb.train('not spam', 'Some more strings that are not spam!', (err) => {
           t.notOk(err, 'no err')
-          nb.classifyLabels('Great opportunity in Nigeria!', function (err, categories) {
+          nb.classifyLabels('Great opportunity in Nigeria!', (err, categories) => {
             t.same(categories[0].label, 'spam', 'should be spam')
             t.same(categories[1].label, 'dont know', 'dont know is second guess')
             t.same(categories[2].label, 'not spam', 'last possible guess')
@@ -116,14 +114,14 @@ tape('classifyLabels with three labels', function (t) {
   })
 })
 
-tape('classifyLabels with promises', testClasifyLabels)
+tape('classifyLabels with promises', testClassifyLabels)
 
-function testClasifyLabels(t) {
+function testClassifyLabels(t) {
   var nb = bayes(memdb())
 
   var thingsToDo = [
     nb.trainAsync('positive', 'Sweet, this is incredibly, amazing, perfect, great!!'),
-    nb.trainAsync('neutral', 'Additionally, the cost of computing the addition can be avoided in some situations'),
+    nb.trainAsync('neutral', 'the cost of computing can be avoided in some situations'),
     nb.trainAsync('neutral', 'many applications a multiplication of probabilities'),
     nb.trainAsync('positive', 'amazing, awesome movie!! Yeah!! Oh boy.'),
     nb.trainAsync('negative', 'terrible, shitty thing. Damn. Sucks!!'),
@@ -131,9 +129,9 @@ function testClasifyLabels(t) {
   ];
 
   q.all(thingsToDo)
-    .then(() => (nb.classifyLabelsAsync('awesome, cool, amazing!! Yay.')))
+    .then(() => nb.classifyLabelsAsync('awesome, cool, amazing!! Yay.'))
     .then(labels => t.same('positive', labels[0].label, 'should be positive'))
-    .then(() => nb.classifyLabelsAsync('probabilities are this gives a lower bound'))
+    .then(() => nb.classifyLabelsAsync('probabilities'))
     .then(labels => {
       t.same('neutral', labels[0].label, 'should be neutral')
       t.same('number', typeof labels[0].logProb, 'should be Number')
